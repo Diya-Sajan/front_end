8519069168192291642793ab3103a031fa0b8acb
Diya-Sajan <diyasajan.2301@gmail.com>
2023-10-08 15:38:46 +0530

    day 1 - still learning

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("hey you!");
-   
+
+let person={
+    name: "diya",
+    age: 21,
+};
+person.name = 'dooya';
+person['name'] = 'deeeya';
+
+let items = ["diya", "sajan"];
+items[3] = "disha"
+
+console.log(items.keys)
+
+//performing a task
+function greet(name){
+    console.log("hey there "+name)
+}
+
+greet("ding ding")
