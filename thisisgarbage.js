78f50a569f7c0bfb3b88962ca69364b259be77a9
Diya-Sajan <diyasajan.2301@gmail.com>
2023-10-23 23:16:51 +0530

    just random practice

---
+++ thisisgarbage.js
@@ -0,0 +1,19 @@
+//title is that i dont have internet today but i need to make a commit to here it is, my attempt at making a file worthy of a commit
+
+//lets try to do random things using random things and stuff
+
+let num = [1,2,3,0,4,5,6,7,7,9,10];
+num.sort();
+console.log(num);
+
+num.shift();
+console.log(num);
+
+num.splice(1,1);
+console.log(num);
+
+num.splice(7,1,8);
+console.log(num);
+
+
+console.log();
