ef9f3a86917133698409f92bcd4d24fe431ff181
Diya-Sajan <diyasajan.2301@gmail.com>
2023-10-26 23:19:10 +0530

    update on 26-10-23

--- js-arrays-loops.js
+++ js-arrays-loops.js
@@ -4,8 +4,34 @@ for (let i=0;i<num.length;i++){
     console.log(num[i])
 }
 console.log(" ")
+
 num.forEach((i)=>{
     console.log(i*i)
 })
 
- //i need to do more. ugh. ugh. ugh. 
+for (let i in num){
+    console.log(num[i])
+}
+
+//map makes a new modified array. original no change
+let arr = [45,23,81]
+let a = arr.map((va,index,array)=>{
+    console.log(va,index,array)
+    return va + index
+})
+console.log(a)
+
+//filter also makes a new modified array. original no change
+let arr2 = [45,23,12,23,4,55,5,67,]
+a2 = arr2.filter((a)=>{
+    return a<25
+})
+console.log(a2)
+
+//reduce returns a single value, ie it reduces an array into one single balue
+let arr3 = ['d','i','y','a']
+let a3= arr3.reduce((h1,h2)=>{
+    return h1+h2
+})
+console.log(a3)
+
