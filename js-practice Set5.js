fb3efd12ff949a8f70ab042b87156406d7038dd4
Diya-Sajan <diyasajan.2301@gmail.com>
2023-10-27 23:58:10 +0530

    another tiny update

---
+++ js-practice Set5.js
@@ -0,0 +1,8 @@
+//question 1
+let ar1 = []
+let nu1 = prompt("enter the number of elements in the array");
+for (let i=1; i<= nu1;i++){
+    let x = prompt("enter element "+i);
+    ar1.push(x)
+}
+console.log(ar1)
