9bba3caa1a958177cb90cb1f632e0116081d303b
Diya-Sajan <diyasajan.2301@gmail.com>
2023-10-28 00:37:27 +0530

    update 28-10-21

--- js-practice Set5.js
+++ js-practice Set5.js
@@ -1,10 +1,46 @@
 //question 1
-let ar1 = []
-let nu1 = prompt("enter the number of elements in the array");
-for (let i=1; i<= nu1;i++){
-    let x = prompt("enter element "+i);
-    ar1.push(x)
-}
-console.log(ar1)
+// let ar1 = []
+// let nu1 = prompt("enter the number of elements in the array");
+// nu1 = Number.parseInt();
+// for (let i=1; i<= nu1;i++){
+//     let x = prompt("enter element "+i);
+//     ar1.push(x)
+// }
+// console.log(ar1)
 
 //question 2
+// let ar2 = []
+// let nu2 = prompt ("Enter elemet 1");
+// while (nu2!=0){
+//     ar2.push(nu2)
+//     nu2 = prompt("Enter next element ");
+// }
+// ar2.push(nu2)
+// console.log(ar2)
+
+//question 3
+// ar2 = [10,23,20,30,22,33,40,44,400]
+// let ar3 = ar2.filter((a)=>{
+//     if(a % 10 ==0){
+//         return a
+//     }
+// })
+// console.log(ar3)
+
+//question 4
+// ar2 = [1,2,3,4]
+// let ar4 = ar2.map((a)=>{
+//     return a*a
+// })
+// console.log(ar4)
+
+//question 5
+// ar5 = []
+// let n = prompt ("enter number")
+// for(let i=1;i<=n;i++){
+//     ar5.push(i)
+// }
+// let x = ar5.reduce((a,b)=>{
+//     return a*b
+// })
+// console.log(x)
